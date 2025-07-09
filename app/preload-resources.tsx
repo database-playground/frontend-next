f7c0455e7e257c8b8d5b1e107baf1811410120bb
Yi-Jyun Pan <me@pan93.com>
2025-07-09 20:37:31 +0800

    fix: remove 'font' preload of css

--- app/preload-resources.tsx
+++ app/preload-resources.tsx
@@ -5,7 +5,6 @@ import ReactDOM from "react-dom";
 export function PreloadResources() {
   ReactDOM.preconnect("https://assets.dbplay.app");
   ReactDOM.preload("https://assets.dbplay.app/ibm-plex-sans-tc/css/ibm-plex-sans-tc-default.min.css", { as: "style" });
-  ReactDOM.preload("https://assets.dbplay.app/ibm-plex-sans-tc/css/ibm-plex-sans-tc-default.min.css", { as: "font" });
 
   return null;
 }
