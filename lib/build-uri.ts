61dcb270c6dfaf59e50e57e29fcc1cc8d54f7217
Yi-Jyun Pan <me@pan93.com>
2025-08-25 02:46:22 +0800

    refactor: update build-uri to match admin-frontend

--- lib/build-uri.ts
+++ lib/build-uri.ts
@@ -1,5 +1,5 @@
 export default function buildUri(path: string) {
   const normalizedPath = path.replace(/\/+$/, "");
 
-  return new URL(normalizedPath, process.env.NEXT_PUBLIC_BACKEND_URL).toString();
+  return new URL(normalizedPath, process.env.NEXT_PUBLIC_BACKEND_BASE_URL).toString();
 }
