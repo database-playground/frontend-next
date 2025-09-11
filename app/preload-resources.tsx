f1248bae1fa2f7c28a713e6dc00ef811128804a2
Yi-Jyun Pan <me@pan93.com>
2025-09-12 01:45:10 +0800

    style: reformat code

--- app/preload-resources.tsx
+++ app/preload-resources.tsx
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom";
 
 export function PreloadResources() {
   ReactDOM.preconnect("https://assets.dbplay.app");
-  ReactDOM.preload("https://assets.dbplay.app/ibm-plex-sans-tc/css/ibm-plex-sans-tc-default-swap.min.css", { as: "style" });
+  ReactDOM.preload("https://assets.dbplay.app/ibm-plex-sans-tc/css/ibm-plex-sans-tc-default-swap.min.css", {
+    as: "style",
+  });
 
   return null;
 }
