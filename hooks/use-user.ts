faffa3a2180171de56af15208e66e3dc3cad6932
Yi-Jyun Pan <me@pan93.com>
2025-09-16 23:00:19 +0800

    refactor: move use-user to hooks

---
+++ hooks/use-user.ts
@@ -0,0 +1,10 @@
+"use client";
+
+import { BASIC_USER_INFO_QUERY } from "@/lib/user";
+import { useSuspenseQuery } from "@apollo/client/react";
+
+export default function useUser() {
+    const { data } = useSuspenseQuery(BASIC_USER_INFO_QUERY);
+
+    return { user: data?.me };
+}
