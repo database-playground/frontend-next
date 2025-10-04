bdd0d890f4fc90fba2eb454f48af3f0ee4117583
Yi-Jyun Pan <me@pan93.com>
2025-10-05 03:08:00 +0800

    fix: reactCompiler is not experimental now

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  reactCompiler: true,
   experimental: {
-    reactCompiler: true,
     viewTransition: true,
     swcPlugins: [
       ["@swc-contrib/plugin-graphql-codegen-client-preset", { artifactDirectory: "./gql", gqlTagName: "graphql" }],
