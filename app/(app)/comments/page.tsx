6feb86707937fd721ceedba52208d27262fd2770
Yi-Jyun Pan <me@pan93.com>
2025-10-07 03:03:33 +0800

    feat: comments WIP page

--- app/(app)/comments/page.tsx
+++ app/(app)/comments/page.tsx
@@ -1,3 +1,7 @@
+import PageHeader from "@/components/page-header";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { AlertCircle } from "lucide-react";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -5,5 +9,19 @@ export const metadata: Metadata = {
 };
 
 export default function CommentsPage() {
-  return <div>CommentsPage</div>;
+  return (
+    <section className="space-y-6">
+      <PageHeader title="經驗分享" description="分享您的學習心得和經驗。" />
+
+      <Alert>
+        <AlertCircle />
+        <AlertTitle>功能正在開發</AlertTitle>
+        <AlertDescription>
+          目前尚未上線分享學習經驗的功能。在服務上線之前，歡迎提供您對這個功能的意見！
+        </AlertDescription>
+      </Alert>
+
+      <Button variant="outline" id="comment-feedback-button">提供意見</Button>
+    </section>
+  );
 }
