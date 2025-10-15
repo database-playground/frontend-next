7216ec4cb01f20d8d61a07bee9d3872941d1b763
Yi-Jyun Pan <me@pan93.com>
2025-10-16 01:19:17 +0800

    fix: switch to Next.js flat config
    https://github.com/vercel/next.js/pull/84874

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,14 @@
-import { FlatCompat } from "@eslint/eslintrc";
 import eslintParserTypeScript from "@typescript-eslint/parser";
+import coreWebVitals from "eslint-config-next/core-web-vitals";
+import typescript from "eslint-config-next/typescript";
 import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
 
 const eslintConfig = [
   {
     ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...coreWebVitals,
+  ...typescript,
   {
     files: ["**/*.{ts,tsx,cts,mts}"],
     languageOptions: {
